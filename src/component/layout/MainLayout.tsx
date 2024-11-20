2ad95752bf143afd978b9e48f6cafaaf4b011802
RakibMojumder <sujonahmed45a4@gmail.com>
2024-11-20 16:09:27 +0600

    feat:Footer section added

--- src/component/layout/MainLayout.tsx
+++ src/component/layout/MainLayout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 const MainLayout = () => {
   return (
     <>
       <div className="h-20">Nav</div>
       <Outlet />
-
-      <h1>footer</h1>
+      <Footer />
     </>
   );
 };
