ebf6c5b8dc7fa5255d167a6a2c7c9a2c99eb2dab
touhidcodes <touhidcodes@gmail.com>
2024-11-22 18:54:05 +0600

    fix: ts build error fixed

--- src/component/layout/DashboardLayout.tsx
+++ src/component/layout/DashboardLayout.tsx
@@ -8,7 +8,6 @@ import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
