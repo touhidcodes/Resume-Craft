5890adf3d8fc07b0a0360299f3a6710cbef7573d
RakibMojumder <sujonahmed45a4@gmail.com>
2024-11-24 20:32:01 +0600

    feat: all resumes page added

--- src/routes/userroute.tsx
+++ src/routes/userroute.tsx
@@ -1,9 +1,21 @@
+import AllResumes from "../pages/dashboard/user/AllResumes";
 import UserDashboard from "../pages/dashboard/user/UserDashboard";
+import UserProfile from "../pages/dashboard/user/UserProfile";
 
 export const userPath = [
   {
     name: "Dashboard",
     path: "dashboard",
     element: <UserDashboard />,
+  },
+  {
+    name: "Resumes",
+    path: "resumes",
+    element: <AllResumes />,
+  },
+  {
+    name: "Profile",
+    path: "user-profile",
+    element: <UserProfile />,
   },
 ];
