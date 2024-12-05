bb7191388d9ebf5fa2fce03ad306d864836ac195
Rifat7432 <mdr341762@gmail.com>
2024-12-05 22:33:59 +0600

    update : vercel link added

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: "https://resume-craft-server-theta.vercel.app/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
