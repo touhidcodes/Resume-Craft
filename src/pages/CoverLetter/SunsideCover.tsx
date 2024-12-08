5465ece66489ebb2ab1a2fd1a0681f12c538c73c
RakibMojumder <sujonahmed45a4@gmail.com>
2024-12-08 19:25:56 +0600

    update: cover letter folder modified

--- src/pages/CoverLetter/SunsideCover.tsx
+++ src/pages/CoverLetter/SunsideCover.tsx
@@ -1,11 +1,11 @@
-import Body from "../../component/CoverLetterSection/Body";
-import Footer from "../../component/CoverLetterSection/Footer";
-import HeaderCl from "../../component/CoverLetterSection/HeaderCl";
+import Body from "../../component/coverLetterSection/sunside/Body";
+import Footer from "../../component/coverLetterSection/sunside/Footer";
+import Header from "../../component/coverLetterSection/sunside/Header";
 
 const SunsideCover = () => {
   return (
-    <div className="min-h-[590px] max-w-[800px] mx-auto rounded-[9px] border my-[20px] px-[85px] py-[54px]  ">
-      <HeaderCl></HeaderCl>
+    <div className="min-h-[590px] max-w-[800px] mx-auto rounded-[9px] border my-[20px] px-[85px] py-[54px]">
+      <Header></Header>
       <Body />
       <Footer />
     </div>
